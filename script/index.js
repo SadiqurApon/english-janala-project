f22b93dfc726f3ff8c3bc55c34810c9ef8846292
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2026-02-13 17:27:46 +0600

    word section's card er kaj colche

--- script/index.js
+++ script/index.js
@@ -3,15 +3,36 @@ const loadLassons = () => {
         .then((res) => res.json())
         .then((json) => displayLessons(json.data));
 };
+const loadLevelWord = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then(data => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+    // console.log(words);
+    const wordContainer = document.getElementById("word-container");
+    // wordContainer.innerHTML = "";
+    words.forEach(word => {
+        console.log(word);
+        const cardwordDiv = document.createElement("div");
+        cardwordDiv.innerHTML = ` 
+        <p>cat</p>
+        `
+        wordContainer.append(cardwordDiv);
+    });
+};
 
 const displayLessons = (lessons) => {
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
 
-    for(const lesson of lessons){
+    for (const lesson of lessons) {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary"><i class="fa-sharp fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-sharp fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
         `
         levelContainer.appendChild(btnDiv);
     }
